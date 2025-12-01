01452e0f1bc687c8ace7593c68abccb4be2cec84
Aryan <aryan.tamboli@mitaoe.ac.in>
2025-12-01 10:13:40 +0530

    Add Vercel URL to CORS whitelist

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,10 @@ connectDB();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: [
+    'http://localhost:5173',
+    'https://anime-explorer-six.vercel.app'
+  ],
   credentials: true
 }));
 app.use(express.json());
